bfc29bbb546fc5f4f2d8053a7241b93f8ecce8f3
huynhphong1611 <huynhvanphong123456@gmail.com>
2020-05-11 22:57:43 +0700

    khoi tao project tesst

---
+++ WebBuyFood/Content/js/index.js
@@ -0,0 +1,5 @@
+function onClickMenuHeader() {
+    console.log("Bạn đã click vào menu header ");
+    var element = document.getElementById("actionbar-menu");
+    element.classList.toggle("action-bar-menu-hide");
+}
---
+++ WebBuyFood/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebBuyFood.MvcApplication" Language="C#" %>
